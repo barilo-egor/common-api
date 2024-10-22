13cb62de4715232bcc5835e6a45834d75b03a19d
barilo <barilo>
2024-10-22 10:58:57 +0300

    Initial commit

---
+++ src/main/groovy/tgb/cryptoexchange/Main.groovy
@@ -0,0 +1,5 @@
+package tgb.cryptoexchange
+
+static void main(String[] args) {
+    println "Hello world!"
+}
---
+++ src/main/java/tgb/cryptoexchange/CryptoCurrency.java
@@ -0,0 +1,23 @@
+package tgb.cryptoexchange;
+
+/**
+ * Константы криптовалют
+ */
+public enum CryptoCurrency {
+    /**
+     * Криптовалюта BITCOIN
+     */
+    BITCOIN,
+    /**
+     * Криптовалюта LITECOIN
+     */
+    LITECOIN,
+    /**
+     * Криптовалюта USDT
+     */
+    USDT,
+    /**
+     * Криптовалюта MONERO
+     */
+    MONERO
+}
