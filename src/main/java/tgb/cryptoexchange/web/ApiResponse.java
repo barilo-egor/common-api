27597869ecdb6aae3b4d38011e189ef23567e8a9
barilo <e.barilo@isida.by>
2024-10-22 13:25:56 +0300

    non null mapping

--- src/main/java/tgb/cryptoexchange/web/ApiResponse.java
+++ src/main/java/tgb/cryptoexchange/web/ApiResponse.java
@@ -1,8 +1,10 @@
 package tgb.cryptoexchange.web;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
 
 @Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class ApiResponse<T> {
 
     private boolean success;
