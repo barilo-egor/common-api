8e8576d29b1852b61320e5a7937895b834a9a391
barilo <barilo>
2024-10-23 00:14:34 +0300

    ApiController java docs

--- src/main/java/tgb/cryptoexchange/controller/ApiController.java
+++ src/main/java/tgb/cryptoexchange/controller/ApiController.java
@@ -6,9 +6,17 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import tgb.cryptoexchange.web.ApiResponse;
 
+/**
+ * Базовый контроллер для REST API
+ */
 @Slf4j
 public abstract class ApiController {
 
+    /**
+     * Обработка ошибок
+     * @param ex исключение не ообработанное на контроллере
+     * @return ответ с ошибкой
+     */
     @ExceptionHandler
     public ResponseEntity<ApiResponse<Object>> handleException(Exception ex){
         log.debug("Необработнная ошибка: ", ex);
