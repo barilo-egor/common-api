c6f1959492d5e2d29b4c76731ccc32c766a981f7
barilo <e.barilo@isida.by>
2024-10-22 13:44:58 +0300

    java docs

--- src/main/java/tgb/cryptoexchange/web/ApiResponse.java
+++ src/main/java/tgb/cryptoexchange/web/ApiResponse.java
@@ -3,22 +3,50 @@ package tgb.cryptoexchange.web;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
 
+/**
+ * Класс для API ответов
+ * @param <T> тип данных ответа
+ */
 @Data
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class ApiResponse<T> {
 
+    /**
+     * Указывает, был ли запрос успешен.
+     */
     private boolean success;
 
+    /**
+     * Данные ответа
+     */
     private T data;
 
+    /**
+     * Ошибка в случае неудачной обработки запроса
+     */
     private Error error;
 
+    /**
+     * Создание пустого объекта ответа
+     */
+    public ApiResponse() {
+    }
+
+    /**
+     * Класс описывающий ошибку при обработке запроса
+     */
     @Data
     public static class Error {
 
         private String message;
     }
 
+    /**
+     * Метод для создания успешного ответа
+     * @param data данные ответа
+     * @return объект успешного ответа с данными
+     * @param <T> тип данных
+     */
     public static <T> ApiResponse<T> success(T data) {
         ApiResponse<T> apiResponse = new ApiResponse<>();
         apiResponse.setSuccess(true);
@@ -26,6 +54,12 @@ public class ApiResponse<T> {
         return apiResponse;
     }
 
+    /**
+     * Метод для создания ответа с ошибкой
+     * @param error объект описывающий ошибку
+     * @return объект ответа с ошибкой
+     * @param <T> тип данных
+     */
     public static <T> ApiResponse<T> error(Error error) {
         ApiResponse<T> apiResponse = new ApiResponse<>();
         apiResponse.setSuccess(false);
