8dc3a5c47304b426712cee371f7dc243eacc3e03
barilo <barilo>
2026-01-21 20:44:07 +0300

    Обработка UnprocessableEntityException

---
+++ src/main/java/tgb/cryptoexchange/exception/UnprocessableEntityException.java
@@ -0,0 +1,15 @@
+package tgb.cryptoexchange.exception;
+
+public class UnprocessableEntityException extends RuntimeException {
+
+    public UnprocessableEntityException() {
+    }
+
+    public UnprocessableEntityException(String message) {
+        super(message);
+    }
+
+    public UnprocessableEntityException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
